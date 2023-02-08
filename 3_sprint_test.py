404f40934af7780ca187f48ce7e9e0e1a157c4c0
Александр Жданов <canshok89@gmail.com>
2023-02-08 09:47:36 +0500

    Переинование колонок

--- 3_sprint_test.py
+++ 3_sprint_test.py
@@ -246,3 +246,18 @@ ADD COLUMN is_on_main INTEGER;
 
 con.commit()
 con.close()
+
+
+# Переименование колонки
+import sqlite3
+
+con = sqlite3.connect('db.sqlite')
+cur = con.cursor()
+
+cur.executescript('''
+ALTER TABLE ice_cream
+RENAME COLUMN description TO specification;
+''')
+
+con.commit()
+con.close()
